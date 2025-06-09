a0023eab9681ab7ad25f219b606a03028916a016
daehyuk1231 <daehyuk1231@naver.com>
2025-06-09 10:15:54 +0900

    OpTest3

---
+++ src/operation/OpTest3.java
@@ -0,0 +1,15 @@
+package operation;
+
+public class OpTest3 {
+	public static void main(String[] args) {
+		boolean b1 = true, b2 = false;
+
+		System.out.println(b1 && b2);
+		System.out.println(b1 || b2);
+		System.out.println(!b1);
+
+		int n1 = 5, n2 = 3;
+		System.out.println(n1 & n2);
+		System.out.println(n1 | n2);
+	}
+}
