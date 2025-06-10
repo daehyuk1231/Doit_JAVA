7d11946678bc53587a748bf6fbfdfea33f1fd4f2
daehyuk1231 <daehyuk1231@naver.com>
2025-06-10 11:03:33 +0900

    ContinueControlKeyword

--- src/control/RockPaperScissors.java
+++ src/control/RockPaperScissors.java
@@ -13,38 +13,34 @@ public class RockPaperScissors {
 
 		switch (you) {
 		case 1: {
+			System.out.println("com > " + com);
 			if (com == 1) {
-				System.out.println("com > " + com);
 				System.out.println("비겼습니다.");
 			} else if (com == 2) {
-				System.out.println("com > " + com);
 				System.out.println("졌습니다.");
 			} else {
-				System.out.println("com > " + com);
 				System.out.println("이겼습니다.");
 			}
+			break;
 		}
 		case 2: {
+			System.out.println("com > " + com);
 			if (com == 1) {
-				System.out.println("com > " + com);
 				System.out.println("이겼습니다.");
 			} else if (com == 2) {
-				System.out.println("com > " + com);
 				System.out.println("비겼습니다.");
 			} else {
-				System.out.println("com > " + com);
 				System.out.println("졌습니다.");
 			}
+			break;
 		}
 		case 3: {
+			System.out.println("com > " + com);
 			if (com == 1) {
-				System.out.println("com > " + com);
 				System.out.println("졌습니다.");
 			} else if (com == 2) {
-				System.out.println("com > " + com);
 				System.out.println("이겼습니다.");
 			} else {
-				System.out.println("com > " + com);
 				System.out.println("비겼습니다.");
 			}
 		}
