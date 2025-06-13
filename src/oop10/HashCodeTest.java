1eb661ebb0107115e68e7ca440815d59681a8e9b
daehyuk1231 <daehyuk1231@naver.com>
2025-06-13 17:34:02 +0900

    ObjectMethod_hashcode

---
+++ src/oop10/HashCodeTest.java
@@ -0,0 +1,11 @@
+package oop10;
+
+class Point {
+}
+
+public class HashCodeTest {
+	public static void main(String[] args) {
+		Point p = new Point();
+		System.out.printf("%x", p.hashCode());
+	}
+}
