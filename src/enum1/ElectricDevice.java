1eb06ac6d5643be5427f12afe80cca1a1dd5c08c
daehyuk1231 <daehyuk1231@naver.com>
2025-06-18 15:45:03 +0900

    enum

---
+++ src/enum1/ElectricDevice.java
@@ -0,0 +1,7 @@
+package enum1;
+
+public abstract class ElectricDevice {
+	public abstract void turnOn();
+
+	public abstract void turnOff();
+}
---
+++ src/enum1/PowerStateTest.java
@@ -0,0 +1,13 @@
+package enum1;
+
+public class PowerStateTest {
+	public static void main(String[] args) {
+		PowerState state = PowerState.SUSPEND;
+		// 필드 값 접근
+		System.out.println("현재 상태: " + state.name());
+		System.out.println("상세 메시지: " + state.getMessage());
+		// 메서드 호출
+		state.printStatus();
+		System.out.println(state.ordinal());
+	}
+}
