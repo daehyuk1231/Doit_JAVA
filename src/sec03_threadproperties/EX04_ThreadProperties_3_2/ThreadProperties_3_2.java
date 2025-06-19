e0dc09e1112346637333219bf40d52c5a77ca154
daehyuk1231 <daehyuk1231@naver.com>
2025-06-19 14:43:47 +0900

    ThreadProperties

---
+++ src/sec03_threadproperties/EX04_ThreadProperties_3_2/ThreadProperties_3_2.java
@@ -0,0 +1,31 @@
+package sec03_threadproperties.EX04_ThreadProperties_3_2;
+
+class MyThread extends Thread {
+	@Override
+	public void run() {
+		System.out.println(getName() + ": " + (isDaemon() ? "데몬 쓰레드" : "일반 쓰레드"));
+		for (int i = 0; i < 6; i++) {
+			System.out.println(getName() + ": " + i + "초");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+}
+
+public class ThreadProperties_3_2 {
+	public static void main(String[] args) {
+
+		Thread thread2 = new MyThread();
+		thread2.setDaemon(true);
+		thread2.setName("thread2");
+		thread2.start();
+
+		try {
+			Thread.sleep(3500);
+		} catch (InterruptedException e) {
+		}
+		System.out.println("main Thread 종료");
+	}
+}
