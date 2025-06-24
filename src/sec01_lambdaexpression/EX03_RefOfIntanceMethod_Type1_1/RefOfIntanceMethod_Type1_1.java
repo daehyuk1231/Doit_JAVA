3af52c6957472f2687792e711a092bf10b47e079
daehyuk1231 <daehyuk1231@naver.com>
2025-06-24 10:36:32 +0900

    Lambda

---
+++ src/sec01_lambdaexpression/EX03_RefOfIntanceMethod_Type1_1/RefOfIntanceMethod_Type1_1.java
@@ -0,0 +1,32 @@
+package sec01_lambdaexpression.EX03_RefOfIntanceMethod_Type1_1;
+
+interface A {
+	void abc();
+}
+
+class B {
+	void bcd() {
+		System.out.println("메서드");
+	}
+}
+
+public class RefOfIntanceMethod_Type1_1 {
+	public static void main(String[] args) {
+		A a1 = new A() {
+			@Override
+			public void abc() {
+				B b = new B();
+				b.bcd();
+			}
+		};
+		A a2 = () -> {
+			B b = new B();
+			b.bcd();
+		};
+		B b = new B();
+		A a3 = b::bcd;
+		a1.abc();
+		a2.abc();
+		a3.abc();
+	}
+}
